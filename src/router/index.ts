382eb9180acc535a99751174fce679a4f7c8aaad
bitbrain <miguel-gonzalez@gmx.de>
2025-05-10 16:07:28 +0100

    Ensure vite app uses correct sub-path /eisenfaust

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,27 +2,26 @@ import { createRouter, createMemoryHistory, RouteRecordRaw } from 'vue-router'
 import LandingView from '../views/LandingView.vue'
 import MediaView from '../views/MediaView.vue'
 import NewsView from '../views/NewsView.vue'
+
 export const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'landing',
     component: LandingView
-    },
-    {
-      path: '/media',
-      name: 'media',
-      component: MediaView
-    },
-    {
-      path: '/news',
-      name: 'news',
-      component: NewsView
-    }
+  },
+  {
+    path: '/media',
+    name: 'media',
+    component: MediaView
+  },
+  {
+    path: '/news',
+    name: 'news',
+    component: NewsView
+  }
 ]
 
-const router = createRouter({
+export default createRouter({
   history: createMemoryHistory(import.meta.env.BASE_URL),
   routes
 })
-
-export default router
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
+  base: '/eisenfaust',
   plugins: [
     vue(),
     tsconfigPaths(),
